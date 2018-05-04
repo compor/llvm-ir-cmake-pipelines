67995fd55f0e7dd67fc15c3df07fce147aba9eb0
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2018-05-04 12:53:07 +0100

    remove installation part

--- cmake/LoopC14N.cmake
+++ cmake/LoopC14N.cmake
@@ -47,32 +47,5 @@ function(LoopC14NPipeline trgt)
     ${PIPELINE_PREFIX}_bc_exe)
 
   add_dependencies(${PIPELINE_NAME} ${PIPELINE_SUBTARGET})
-
-
-  # installation
-  get_property(bmk_name TARGET ${trgt} PROPERTY BMK_NAME)
-  set(DEST_DIR "${bmk_name}")
-
-  install(TARGETS ${PIPELINE_PREFIX}_bc_exe
-    DESTINATION ${DEST_DIR} OPTIONAL)
-
-  set(BMK_BIN_NAME "${PIPELINE_PREFIX}_bc_exe")
-  set(BMK_BIN_PREAMBLE "")
-  set(PIPELINE_SCRIPT_PREFIX "${PIPELINE_NAME}")
-
-  configure_file("scripts/_run.sh.in" "scripts/${PIPELINE_PREFIX}_run.sh" @ONLY)
-
-  install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/scripts/
-    DESTINATION ${DEST_DIR}
-    PATTERN "*.sh"
-    PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE)
-
-  # IR installation
-  if(NOT TARGET ${PIPELINE_INSTALL_TARGET})
-    add_custom_target(${PIPELINE_INSTALL_TARGET})
-  endif()
-
-  InstallPipelineLLVMIR(DEPENDS ${PIPELINE_PREFIX}_link
-    ATTACH_TO_TARGET ${PIPELINE_INSTALL_TARGET} BMK_NAME ${bmk_name})
 endfunction()
 
