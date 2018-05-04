ea2e75b1f7f6763442f1a62ab4c62d503926cc2d
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2018-05-04 14:42:25 +0100

    provide in and out arguments

--- cmake/LoopC14N.cmake
+++ cmake/LoopC14N.cmake
@@ -1,22 +1,64 @@
 # cmake file
 
-function(LoopC14NPipeline trgt)
-  set(PIPELINE_NAME "LoopC14N")
-  set(PIPELINE_INSTALL_TARGET "${PIPELINE_NAME}-install")
+include(CMakeParseArguments)
 
-  if(NOT TARGET ${PIPELINE_NAME})
-    add_custom_target(${PIPELINE_NAME})
+function(LoopC14NPipeline)
+  set(PIPELINE_NAME "LoopC14N")
+  string(TOUPPER "${PIPELINE_NAME}" PIPELINE_NAME_UPPER)
+
+  set(options ALL)
+  set(oneValueArgs NAME DEPENDS MAIN_TARGET TARGET_LIST)
+  set(multiValueArgs)
+  cmake_parse_arguments(${PIPELINE_NAME_UPPER}
+    "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+  set(NAME ${${PIPELINE_NAME_UPPER}_NAME})
+  set(TRGT ${${PIPELINE_NAME_UPPER}_DEPENDS})
+  set(MAIN_TRGT ${${PIPELINE_NAME_UPPER}_MAIN_TARGET})
+  set(TRGT_LIST ${${PIPELINE_NAME_UPPER}_TARGET_LIST})
+
+  # argument checks
+
+  if(NOT NAME)
+    set(NAME "${PIPELINE_NAME}")
   endif()
 
-  set(PIPELINE_SUBTARGET "${PIPELINE_NAME}_${trgt}")
-  set(PIPELINE_PREFIX ${PIPELINE_SUBTARGET})
+  if(${PIPELINE_NAME_UPPER}_ALL)
+    set(ALL_OPTION "ALL")
+  endif()
 
-  ## pipeline targets and chaining
+  if(NOT MAIN_TRGT)
+    message(FATAL_ERROR "pipeline ${PIPELINE_NAME}: missing MAIN_TARGET target")
+  endif()
+
+  if(NOT TRGT)
+    message(FATAL_ERROR "pipeline ${PIPELINE_NAME}: missing DEPENDS target")
+  endif()
+
+  if(NOT TARGET ${TRGT})
+    message(FATAL_ERROR "pipeline ${PIPELINE_NAME}: ${TRGT} is not a target")
+  endif()
+
+  if(${PIPELINE_NAME_UPPER}_UNPARSED_ARGUMENTS)
+    message(FATAL_ERROR "pipeline ${PIPELINE_NAME}: has extraneous arguments \
+    ${${PIPELINE_NAME_UPPER}_UNPARSED_ARGUMENTS}")
+  endif()
+
+  # set up targets
+
+  if(NOT TARGET ${NAME})
+    add_custom_target(${NAME} ${ALL_OPTION})
+  endif()
+
+  set(PIPELINE_SUBTARGET "${NAME}_${TRGT}")
+  set(PIPELINE_PREFIX "${PIPELINE_SUBTARGET}")
+
+  # pipeline targets and chaining
 
   llvmir_attach_bc_target(
     TARGET ${PIPELINE_PREFIX}_bc
-    DEPENDS ${trgt})
-  add_dependencies(${PIPELINE_PREFIX}_bc ${trgt})
+    DEPENDS ${TRGT})
+  add_dependencies(${PIPELINE_PREFIX}_bc ${TRGT})
 
   llvmir_attach_opt_pass_target(
     TARGET ${PIPELINE_PREFIX}_opt
@@ -37,15 +79,33 @@ function(LoopC14NPipeline trgt)
     DEPENDS ${PIPELINE_PREFIX}_link)
   add_dependencies(${PIPELINE_PREFIX}_bc_exe ${PIPELINE_PREFIX}_link)
 
-  target_link_libraries(${PIPELINE_PREFIX}_bc_exe m)
+  # TODO see if this is needed
+  #target_link_libraries(${PIPELINE_PREFIX}_bc_exe m)
 
-  ## pipeline aggregate targets
+  # aggregate targets for pipeline
+
   add_custom_target(${PIPELINE_SUBTARGET} DEPENDS
     ${PIPELINE_PREFIX}_bc
     ${PIPELINE_PREFIX}_opt
     ${PIPELINE_PREFIX}_link
     ${PIPELINE_PREFIX}_bc_exe)
 
-  add_dependencies(${PIPELINE_NAME} ${PIPELINE_SUBTARGET})
+  set(INTERNAL_TRGT_LIST "")
+  list(APPEND INTERNAL_TRGT_LIST
+    ${PIPELINE_SUBTARGET}
+    ${PIPELINE_PREFIX}_bc
+    ${PIPELINE_PREFIX}_opt
+    ${PIPELINE_PREFIX}_link
+    ${PIPELINE_PREFIX}_bc_exe)
+
+  add_dependencies(${NAME} ${PIPELINE_SUBTARGET})
+
+  # export targets
+
+  set(${MAIN_TRGT} "${PIPELINE_PREFIX}_link" PARENT_SCOPE)
+
+  if(TRGT_LIST)
+    set(${TRGT_LIST} "${INTERNAL_TRGT_LIST}" PARENT_SCOPE)
+  endif()
 endfunction()
 
