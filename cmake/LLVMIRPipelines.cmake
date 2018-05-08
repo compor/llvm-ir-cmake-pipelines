423730c553e7b88ce567b52f39f4d42a32708eea
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2018-05-08 16:55:27 +0100

    use function to setup pipelines

--- cmake/LLVMIRPipelines.cmake
+++ cmake/LLVMIRPipelines.cmake
@@ -8,51 +8,88 @@ cmake_minimum_required(VERSION 3.2)
 
 message(STATUS "LLVM IR Pipelines")
 
-include(${CMAKE_CURRENT_LIST_DIR}/LLVMIRPipelinesGenerators.cmake)
+include(CMakeParseArguments)
 
-if(LLVMIR_PIPELINES_TO_INCLUDE)
-  set(PIPELINE_FILES "${LLVMIR_PIPELINES_TO_INCLUDE}")
-  string(TOUPPER "${PIPELINE_FILES}" PIPELINE_FILES_UPPER)
+set(_THIS_LIST_DIR "${CMAKE_CURRENT_LIST_DIR}")
 
-  list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/pipelines/")
+include(${_THIS_LIST_DIR}/LLVMIRPipelinesGenerators.cmake)
 
-  if("ALL" STREQUAL ${PIPELINE_FILES_UPPER})
-    file(GLOB PIPELINE_FILES
-      RELATIVE "${CMAKE_CURRENT_LIST_DIR}/pipelines/"
-      "${CMAKE_CURRENT_LIST_DIR}/pipelines/*.cmake")
+function(llvmir_pipelines_setup)
+  set(options)
+  set(oneValueArgs DEPENDS OUTPUT_FILE)
+  set(multiValueArgs)
+  cmake_parse_arguments(LPS
+    "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+  if(NOT LPS_DEPENDS)
+    message(FATAL_ERROR "setup command is missing DEPENDS option")
   endif()
 
-  foreach(FILE ${PIPELINE_FILES})
-    message(STATUS "Including pipeline: ${FILE}")
+  if(NOT LPS_OUTPUT_FILE)
+    message(FATAL_ERROR "setup command is missing OUTPUT_FILE option")
+  endif()
 
-    include("${FILE}")
-  endforeach()
-else()
-  message(WARNING "No pipelines included")
-endif()
+  if(NOT LLVMIR_PIPELINES_TO_INCLUDE)
+    message(WARNING "No pipelines to be included using variable: \
+    LLVMIR_PIPELINES_TO_INCLUDE")
+  endif()
 
-#
+  if(NOT LLVMIR_COMPOUND_PIPELINES)
+    message(WARNING "No compound pipelines specified using variable:\
+    LLVMIR_COMPOUND_PIPELINES")
+  endif()
 
-if(LLVMIR_COMPOUND_PIPELINES)
-  foreach(CPLINE ${LLVMIR_COMPOUND_PIPELINES})
-    string(TOUPPER "${CPLINE}" CPLINE_UC)
-    set(CPLINE_PARTS "LLVMIR_COMPOUND_PIPELINE_${CPLINE_UC}")
+  if(LLVMIR_PIPELINES_TO_INCLUDE)
+    set(PIPELINE_FILES "${LLVMIR_PIPELINES_TO_INCLUDE}")
+    string(TOUPPER "${PIPELINE_FILES}" PIPELINE_FILES_UPPER)
 
-    if(NOT DEFINED ${CPLINE_PARTS})
-      message(FATAL_ERROR "pipeline ${CPLINE_PARTS} is not defined!")
+    list(APPEND CMAKE_MODULE_PATH "${_THIS_LIST_DIR}/pipelines/")
+    set(CMAKE_MODULE_PATH "${CMAKE_MODULE_PATH}" PARENT_SCOPE)
+
+    if("ALL" STREQUAL ${PIPELINE_FILES_UPPER})
+      file(GLOB PIPELINE_FILES
+        RELATIVE "${_THIS_LIST_DIR}/pipelines/"
+        "${_THIS_LIST_DIR}/pipelines/*.cmake")
     endif()
 
-    set(CPLINE_PARTS_CONTENTS "${${CPLINE_PARTS}}")
+    foreach(FILE ${PIPELINE_FILES})
+      message(STATUS "Including pipeline: ${FILE}")
 
-    generate_compound_pipeline_lists(
-      COMPOUND_PIPELINE ${CPLINE}
-      PIPELINES ${CPLINE_PARTS_CONTENTS}
-      OUTPUT_DIR ${CMAKE_CURRENT_BINARY_DIR})
+      include("${FILE}")
+    endforeach()
+  endif()
+
+  #
+
+  if(LLVMIR_COMPOUND_PIPELINES)
+    set(PIPELINE_FILES_DIR "${CMAKE_CURRENT_BINARY_DIR}/pipelines/")
+    file(MAKE_DIRECTORY "${PIPELINE_FILES_DIR}")
+
+    list(APPEND CMAKE_MODULE_PATH "${PIPELINE_FILES_DIR}")
+    set(CMAKE_MODULE_PATH "${CMAKE_MODULE_PATH}" PARENT_SCOPE)
+
+    foreach(CPLINE ${LLVMIR_COMPOUND_PIPELINES})
+      string(TOUPPER "${CPLINE}" CPLINE_UC)
+      set(CPLINE_PARTS "LLVMIR_COMPOUND_PIPELINE_${CPLINE_UC}")
+
+      if(NOT DEFINED ${CPLINE_PARTS})
+        message(FATAL_ERROR "pipeline ${CPLINE_PARTS} is not defined!")
+      endif()
+
+      set(CPLINE_PARTS_CONTENTS "${${CPLINE_PARTS}}")
+
+      generate_compound_pipeline_lists(
+        COMPOUND_PIPELINE ${CPLINE}
+        PIPELINES ${CPLINE_PARTS_CONTENTS}
+        OUTPUT_DIR ${PIPELINE_FILES_DIR})
+
+      include(${CPLINE})
+    endforeach()
 
     generate_pipeline_runner_lists(
       PIPELINES ${LLVMIR_COMPOUND_PIPELINES}
-      DEPENDS TARGET_HOOK_TO_REPLACE
-      OUTPUT_FILE runner
-      OUTPUT_DIR ${CMAKE_CURRENT_BINARY_DIR})
-  endforeach()
-endif()
+      DEPENDS ${LPS_DEPENDS}
+      OUTPUT_FILE ${LPS_OUTPUT_FILE}
+      OUTPUT_DIR ${PIPELINE_FILES_DIR})
+  endif()
+endfunction()
