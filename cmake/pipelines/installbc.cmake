adff222e5a05c8bdb574320cc06444091e4a249e
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2018-05-14 16:45:48 +0100

    add pipeline

---
+++ cmake/pipelines/installbc.cmake
@@ -0,0 +1,33 @@
+# cmake file
+
+# pipeline that attaches to source level targets and produces LLVM IR
+
+set(_THIS_LIST_DIR "${CMAKE_CURRENT_LIST_DIR}")
+
+include(${_THIS_LIST_DIR}/internal/common.cmake)
+
+function(installbc)
+  # CAUTION
+  # function name and NAME provided below are required to match in order for
+  # things to work, since there is currently no programmatic way of accessing
+  # a function's name
+  pipeline_setup(NAME "installbc" ${ARGV})
+
+  # aggregate targets for pipeline
+  install_llvmir_bc(TARGET ${PLINE_DEPENDS})
+
+  list(APPEND INTERNAL_TARGET_LIST
+    ${PLINE_DEPENDS})
+
+  add_dependencies(${PLINE_SUBTARGET} ${INTERNAL_TARGET_LIST})
+
+  # expose targets
+
+  set(${PLINE_MAIN_TARGET} "${PLINE_DEPENDS}" PARENT_SCOPE)
+
+  if(PLINE_TARGET_LIST)
+    list(APPEND INTERNAL_TARGET_LIST ${PLINE_SUBTARGET})
+    set(${PLINE_TARGET_LIST} "${INTERNAL_TARGET_LIST}" PARENT_SCOPE)
+  endif()
+endfunction()
+
